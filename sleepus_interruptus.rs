0d9593361c389ece7657390f9f26d7ef84c90fc0
Felix Karg <f.karg10@gmail.com>
2019-12-13 13:03:03 +0100

    just some code

---
+++ sleepus_interruptus.rs
@@ -0,0 +1,26 @@
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+
+fn sleepus() {
+    for i in 1..=10 {
+        println!("Sleepus {}", i);
+        sleep(Duration::from_millis(500));
+    }
+}
+
+fn interruptus() {
+    for i in 1..=5 {
+        println!("Interruptus {}", i);
+        sleep(Duration::from_millis(1000));
+    }
+}
+
+fn main() {
+    let sleepus = spawn(sleepus);
+    let interruptus = spawn(interruptus);
+
+    sleepus.join().unwrap();
+    interruptus.join().unwrap();
+
+}
+
